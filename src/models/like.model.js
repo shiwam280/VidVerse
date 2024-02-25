5ff34228d04d9e4eeb9d5546a61465e642ceb02e
Shiwam Singh <shiwamsingh280@gmail.com>
2024-02-25 23:21:23 +0530

    add comment, like, playlist, community models

---
+++ src/models/like.model.js
@@ -0,0 +1,23 @@
+import mongoose, {Schema} from "mongoose";
+
+
+const likeSchema = new Schema({
+    video:{
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    },
+    comment:{
+        type: Schema.Types.ObjectId,
+        ref: "Comment"
+    },
+    community:{
+        type: Schema.Types.ObjectId,
+        ref: "Community"
+    },
+    likedBy:{
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+}, {timestamps: true})
+
+export const Like = mongoose.model("Like", likeSchema);
